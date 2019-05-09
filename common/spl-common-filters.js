a3a4a46754de1e103867eec4d67f3abaf7c24e7c
MatthewBurstein <matthew.burstein@softwire.com>
2019-05-09 14:43:33 +0100

    feat: add common filters for primaryName and isBirth

--- common/spl-common-filters.js
+++ common/spl-common-filters.js
@@ -1,6 +1,16 @@
 // Existing filters can be imported from env using env.getFilter(name)
 // See https://mozilla.github.io/nunjucks/api.html#getfilter
 module.exports = function (env) {
+  function primaryName (data) {
+    return data['birth-or-adoption'] === 'birth' ? 'mother' : 'primary adopter'
+  }
+
+  function isBirth (data) {
+    return data['birth-or-adoption'] === 'birth'
+  }
+
   return {
+    primaryName,
+    isBirth
   }
 }
