debdf66e40a5cf7ba429615e6c17a034a2236859
MatthewBurstein <matthew.burstein@softwire.com>
2019-05-14 16:47:18 +0100

    add formatForDisplay and standardFormat to dateUtils

--- common/utils/dateUtils.js
+++ common/utils/dateUtils.js
@@ -4,11 +4,16 @@ function convertToMoment (year, month, day) {
   return moment([year, month, day].join('-'), 'YYYY-MM-DD')
 }
 
-function formatForDisplay (momentDate) {
-  return momentDate.format('D MMMM YYYY')
+function formatForDisplay (standardFormatDate) {
+  return moment(standardFormatDate).format('D MMMM YYYY')
+}
+
+function standardFormat (momentDate) {
+  return momentDate.format('YYYY-MM-DD')
 }
 
 module.exports = {
   convertToMoment,
-  formatForDisplay
+  formatForDisplay,
+  standardFormat
 }
