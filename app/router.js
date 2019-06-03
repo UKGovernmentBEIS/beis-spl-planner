2b6d1947b9fbf9d4272e19048755daa34875e6e1
MatthewBurstein <matthew.burstein@softwire.com>
2019-06-03 16:04:05 +0100

    add blocks.js to calculate leave and pay blocks

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const paths = require('./paths')
+const { getBlocks } = require('./lib/blocks')
 
 router.route(paths.getPath('root'))
   .get(function (req, res) {
@@ -30,7 +31,8 @@ router.route(paths.getPath('summary'))
   .get(function (req, res) {
     // until the rest of the form is implemented
     Object.assign(req.session.data, { 'start-date-day': '01', 'start-date-month': '09', 'start-date-year': '2019' })
-    res.redirect('back')
+    const { leaveBlocks, payBlocks } = getBlocks(req.session.data)
+    res.render('summary', { leaveBlocks, payBlocks })
   })
 
 module.exports = router
