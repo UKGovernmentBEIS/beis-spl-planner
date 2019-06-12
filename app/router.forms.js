85a33b71bd854058f08aeaa699e1cfd4d867378b
Jamie Humphries <jamie.humphries@softwire.com>
2019-06-12 19:09:01 +0100

    Better fomrs example.

--- app/router.forms.js
+++ app/router.forms.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const nunjucks = require('nunjucks')
+const fileUrl = require('file-url')
 const pdf = require('html-pdf')
+
+const config = {
+  base: fileUrl('app/views/forms') + '/',
+  format: 'A4'
+}
 
 router.get('/example.pdf', function (req, res) {
   nunjucks.render('forms/example.njk', { data: req.session.data }, function (e, html) {
-    pdf.create(html).toStream((e, stream) => stream.pipe(res))
+    pdf.create(html, config).toStream((e, stream) => stream.pipe(res))
   })
 })
 
