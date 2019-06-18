44970b395d85daeeba4c406d11124dde9c0703e6
MatthewBurstein <matthew.burstein@softwire.com>
2019-06-18 16:34:58 +0100

    Add links to eligibility_tool in spl assertions

--- server.js
+++ server.js
@@ -118,6 +118,11 @@ function initialiseTemplateEngine (app) {
   nunjucksEnvironment.addGlobal('css_path', NODE_ENV === 'production' ? CSS_PATH : staticify.getVersionedPath('/stylesheets/application.min.css'))
   nunjucksEnvironment.addGlobal('js_path', NODE_ENV === 'production' ? JAVASCRIPT_PATH : staticify.getVersionedPath('/javascripts/application.js'))
 
+  // Paths to external resources and tools.
+  // TODO: Update fallback with final path of eligibility tool.
+  nunjucksEnvironment.addGlobal('mainstream_guidance_root_path', process.env.MAINSTREAM_GUIDANCE_ROOT_PATH || 'https://www.gov.uk/shared-parental-leave-and-pay')
+  nunjucksEnvironment.addGlobal('eligibility_tool_root_path', process.env.ELIGIBILITY_ROOT_PATH || 'https://beis-spl-eligibility-tool.london.cloudapps.digital')
+
   // Add filters
   const commonFilters = require('./common/spl-common-filters')(nunjucksEnvironment)
   Object.entries(commonFilters).forEach(nameAndFunction => nunjucksEnvironment.addFilter(...nameAndFunction))
