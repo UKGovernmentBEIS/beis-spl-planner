9a1b0b85acfa37d451dfc86d6c3d49adb153ed6b
MatthewBurstein <matthew.burstein@softwire.com>
2019-06-18 14:40:42 +0100

    handle YYYY-MM-DD format in Day constructor

--- common/lib/day.js
+++ common/lib/day.js
@@ -1,13 +1,15 @@
 const moment = require('moment')
 
 class Day {
-  constructor (yearOrMoment, month, day) {
+  constructor (yearDateStringOrMoment, month, day) {
     if (arguments.length === 0) {
       this.moment = moment.utc()
-    } else if (moment.isMoment(yearOrMoment)) {
-      this.moment = yearOrMoment
+    } else if (/^(\d\d)?\d\d-\d\d?-\d\d?$/.test(yearDateStringOrMoment)) {
+      this.moment = moment.utc(yearDateStringOrMoment, 'YYYY-MM-DD')
+    } else if (moment.isMoment(yearDateStringOrMoment)) {
+      this.moment = yearDateStringOrMoment
     } else {
-      this.moment = moment.utc([yearOrMoment, month, day].join('-'), 'YYYY-MM-DD')
+      this.moment = moment.utc([yearDateStringOrMoment, month, day].join('-'), 'YYYY-MM-DD')
     }
   }
 
@@ -53,6 +55,14 @@ class Day {
 
   date () {
     return this.moment.date()
+  }
+
+  monthOneIndexed () {
+    return this.moment.month() + 1
+  }
+
+  year () {
+    return this.moment.year()
   }
 }
 
