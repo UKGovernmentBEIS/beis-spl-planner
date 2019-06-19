79ea0fb57196500148a6f3f77d833d1c14907556
MatthewBurstein <matthew.burstein@softwire.com>
2019-06-19 17:38:38 +0100

    remove paths guard clause from handle-404

--- common/middleware/handle-404.js
+++ common/middleware/handle-404.js
@@ -1,7 +1,3 @@
-const paths = require('../../app/paths')
-
 module.exports = function handle404 (req, res) {
-  if (!paths.getAllPaths().includes(req.url)) {
-    res.status(404).render('error-pages/404-page')
-  }
+  res.status(404).render('error-pages/404-page')
 }
