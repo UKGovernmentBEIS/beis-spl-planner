9b7a9aacd46de28719e7d5ac917b3bdf205f03c9
MatthewBurstein <matthew.burstein@softwire.com>
2019-07-19 16:59:01 +0100

    move PrintYourPlan into vue component

---
+++ app/assets/javascripts/components/PrintYourPlan.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="govuk-heading-m">
+      Print your plan
+    </h2>
+    <p>
+      You can <a
+            onclick="window.print()"
+            class="govuk-link--no-visited-state govuk-body print-hide"
+            href="#"
+            data-ga-hit-type="print_planner"
+            data-ga-field-event_category="planner"
+            data-ga-field-event_action="print_page"
+          >
+        print your plan</a> if you want to refer to it later.
+    </p>
+  </div>
+</template>
