667f54b63670f04bfd4dc773ec94027362cec97f
velimd <duzgunce.vd@gmail.com>
2020-03-13 11:28:32 +0000

    removing comments

--- app/node-email.js
+++ app/node-email.js
@@ -1,22 +1,20 @@
 const nodemailer = require('nodemailer')
 
 const sendMail = async (experience, moreDetails) => {
-  // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     port: 465,
-    secure: true, // use SSL
+    secure: true,
     auth: {
       user: process.env.GMAIL_AUTH_USER,
       pass: process.env.GMAIL_AUTH_PASS
     }
   })
 
-  // send mail with defined transport object
   await transporter.sendMail({
-    from: `SPL Feedback <${process.env.GMAIL_AUTH_USER}>`, // sender address
-    to: process.env.GMAIL_SENDER, // list of receivers
-    subject: 'SPL Feedback', // Subject line
+    from: `SPL Feedback <${process.env.GMAIL_AUTH_USER}>`,
+    to: process.env.GMAIL_SENDER,
+    subject: 'SPL Feedback',
     text:
       `
       What was your experience of the service:
@@ -24,7 +22,7 @@ const sendMail = async (experience, moreDetails) => {
       
       How could we improve this service:
       ${moreDetails}
-      ` // plain text body
+      `
   }, (error, info) => {
     if (error) {
       console.log(error)
