09ca2222917724bb869c4a94d0ddf527f1054c21
louisware <louis.ware@solirius.com>
2024-05-08 16:08:00 +0100

    Renamed and moved test objects to separate files

--- test/unit/lib/weeks.test.js
+++ test/unit/lib/weeks.test.js
@@ -1,145 +1,32 @@
 const { describe, it } = require('mocha')
 const assert = require('chai').assert
-const moment = require('moment')
 // const LeaveTracker = require('../../../app/lib/leaveTracker')
 const { earliestPrimaryLeaveWeek } = require('../../../common/lib/dataUtils')
 // const _ = require('lodash')
 
 const Weeks = require('../../../app/lib/weeks')
 
-const options = {
-  natureOfParenthood: 'birth',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 10, leaveWeeks: [1, 2, 3], payWeeks: [1, 2] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: true,
-      statutoryLeave: true,
-      shpp: true,
-      maternityAllowance: false
-    },
-    secondary: {
-      spl: true,
-      statutoryLeave: false
-    }
-  }
-}
-
-const options2 = {
-  natureOfParenthood: 'surrogacy',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 10, leaveWeeks: [1, 2, 3], payWeeks: [1, 2] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: true,
-      statutoryLeave: true,
-      shpp: true,
-      maternityAllowance: false
-    },
-    secondary: {
-      spl: false,
-      statutoryLeave: false
-    }
-  }
-}
-
-const options3 = {
-  natureOfParenthood: 'birth',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 0, leaveWeeks: [1, 2, 3, 12], payWeeks: [1, 2, 11, 12] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: true,
-      statutoryLeave: true,
-      shpp: true,
-      maternityAllowance: false
-    },
-    secondary: {
-      spl: true,
-      statutoryLeave: false
-    }
-  }
-}
-
-const options4 = {
-  natureOfParenthood: 'birth',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 0, leaveWeeks: [0, 1], payWeeks: [0, 1] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: true,
-      statutoryLeave: true,
-      shpp: true,
-      maternityAllowance: false
-    },
-    secondary: {
-      spl: false,
-      statutoryLeave: false
-    }
-  }
-}
-
-const options5 = {
-  natureOfParenthood: 'birth',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 0, leaveWeeks: [0, 1], payWeeks: [0, 1] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: false,
-      statutoryLeave: true,
-      shpp: false,
-      maternityAllowance: true
-    },
-    secondary: {
-      spl: false,
-      statutoryLeave: true
-    }
-  }
-}
-
-const options6 = {
-  natureOfParenthood: 'birth',
-  typeOfAdoption: undefined,
-  startWeek: moment('2024-01-01'),
-  primary: { firstSplWeek: 0, leaveWeeks: [0, 1], payWeeks: [0, 1] },
-  secondary: { leaveWeeks: [], payWeeks: [] },
-  eligibility: {
-    primary: {
-      spl: false,
-      statutoryLeave: false,
-      shpp: true,
-      maternityAllowance: false
-    },
-    secondary: {
-      spl: false,
-      statutoryLeave: false
-    }
-  }
-}
+// Test data
+const birthWithSpl = require('../../data/weeksTestOptions/birthWithSpl')
+const surrogacyWithSpl = require('../../data/weeksTestOptions/surrogacyWithSpl')
+const birthSplStartsCorrectly = require('../../data/weeksTestOptions/birthSplStartsCorrectly')
+const birthPrimaryLeaveStatutoryPay = require('../../data/weeksTestOptions/birthPrimaryLeaveStatutoryPay')
+const birthMaternityAllowanceOnly = require('../../data/weeksTestOptions/birthMaternityAllowanceOnly')
+const birthShppOnly = require('../../data/weeksTestOptions/birthShppOnly')
 
 describe('Weeks Class', function () {
   describe('Constructor', function () {
     it('should properly initialise with the provided options', function () {
-      const weeks = new Weeks(options)
+      const weeks = new Weeks(birthWithSpl)
 
       assert.equal(weeks.primaryLeaveType, 'maternity')
 
       // Verify that properties are set correctly
-      assert.deepEqual(weeks.startWeek, options.startWeek) // <- 'deepEqual' tests structural equality; does not test whether the operands are the same object, but rather that they're equivalent.
-      assert.deepEqual(weeks.primary, options.primary)
-      assert.deepEqual(weeks.secondary, options.secondary)
+      assert.deepEqual(weeks.startWeek, birthWithSpl.startWeek) // <- 'deepEqual' tests structural equality; does not test whether the operands are the same object, but rather that they're equivalent.
+      assert.deepEqual(weeks.primary, birthWithSpl.primary)
+      assert.deepEqual(weeks.secondary, birthWithSpl.secondary)
       assert.deepEqual(weeks.primaryLeaveType, 'maternity') // <- Based on natureOfParenthood === 'birth'
-      assert.deepEqual(weeks.eligibility, options.eligibility)
+      assert.deepEqual(weeks.eligibility, birthWithSpl.eligibility)
 
       // Check computed properties
       assert.equal(weeks.minimumWeek, earliestPrimaryLeaveWeek({ 'nature-of-parenthood': 'birth', 'type-of-adoption': undefined }))
@@ -149,7 +36,7 @@ describe('Weeks Class', function () {
 
   describe('leaveAndPay method', function () {
     it('should generate 52 weeks of leave data', function () {
-      const weeks = new Weeks(options2)
+      const weeks = new Weeks(surrogacyWithSpl)
       const result = weeks.leaveAndPay()
 
       assert.isArray(result.weeks)
@@ -157,7 +44,7 @@ describe('Weeks Class', function () {
     })
 
     it('should handle primary SPL start correctly', function () {
-      const weeks = new Weeks(options3)
+      const weeks = new Weeks(birthSplStartsCorrectly)
       const result = weeks.leaveAndPay()
       const week13 = result.weeks[12]
 
@@ -167,9 +54,9 @@ describe('Weeks Class', function () {
   })
 
   describe('Eligibility check methods', function () {
-    const weeks = new Weeks(options4)
-    const weeks2 = new Weeks(options5)
-    const weeks3 = new Weeks(options6)
+    const weeks = new Weeks(birthPrimaryLeaveStatutoryPay)
+    const weeks2 = new Weeks(birthMaternityAllowanceOnly)
+    const weeks3 = new Weeks(birthShppOnly)
 
     it('should return true for eligible primary leave week', function () {
       const result = weeks._weekEligibleForPrimaryLeave({ primary: { leave: { text: 'maternity' } } })
@@ -209,7 +96,7 @@ describe('Weeks Class', function () {
   })
 
   describe('Shared Pay or Leave methods', function () {
-    const weeks = new Weeks(options)
+    const weeks = new Weeks(birthWithSpl)
 
     it('should return false for primary shared pay', function () {
       const result = weeks.hasPrimarySharedPayOrLeave()
