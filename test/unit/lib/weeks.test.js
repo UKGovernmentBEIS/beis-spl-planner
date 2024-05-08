3103a4559c718023053f4c1f2e6e1f8cda89d8e4
louisware <louis.ware@solirius.com>
2024-05-08 13:21:18 +0100

    Added more tests to improve branch and function coverage

--- test/unit/lib/weeks.test.js
+++ test/unit/lib/weeks.test.js
@@ -87,6 +87,46 @@ const options4 = {
   }
 }
 
+const options5 = {
+  natureOfParenthood: 'birth',
+  typeOfAdoption: undefined,
+  startWeek: moment('2024-01-01'),
+  primary: { firstSplWeek: 0, leaveWeeks: [0, 1], payWeeks: [0, 1] },
+  secondary: { leaveWeeks: [], payWeeks: [] },
+  eligibility: {
+    primary: {
+      spl: false,
+      statutoryLeave: true,
+      shpp: false,
+      maternityAllowance: true
+    },
+    secondary: {
+      spl: false,
+      statutoryLeave: true
+    }
+  }
+}
+
+const options6 = {
+  natureOfParenthood: 'birth',
+  typeOfAdoption: undefined,
+  startWeek: moment('2024-01-01'),
+  primary: { firstSplWeek: 0, leaveWeeks: [0, 1], payWeeks: [0, 1] },
+  secondary: { leaveWeeks: [], payWeeks: [] },
+  eligibility: {
+    primary: {
+      spl: false,
+      statutoryLeave: false,
+      shpp: true,
+      maternityAllowance: false
+    },
+    secondary: {
+      spl: false,
+      statutoryLeave: false
+    }
+  }
+}
+
 describe('Weeks Class', function () {
   describe('Constructor', function () {
     it('should properly initialise with the provided options', function () {
@@ -128,6 +168,8 @@ describe('Weeks Class', function () {
 
   describe('Eligibility check methods', function () {
     const weeks = new Weeks(options4)
+    const weeks2 = new Weeks(options5)
+    const weeks3 = new Weeks(options6)
 
     it('should return true for eligible primary leave week', function () {
       const result = weeks._weekEligibleForPrimaryLeave({ primary: { leave: { text: 'maternity' } } })
@@ -135,8 +177,42 @@ describe('Weeks Class', function () {
       assert.isTrue(result)
     })
 
+    it('should return true for eligible statutory leave week', function () {
+      const result = weeks2._weekEligibleForPrimaryLeave({ primary: { leave: { text: 'maternity' } } })
+
+      assert.isTrue(result)
+    })
+
+    it('should return false for ineligible statutory leave week', function () {
+      const result = weeks3._weekEligibleForPrimaryLeave({ primary: { leave: { text: 'maternity' } } })
+
+      assert.isFalse(result)
+    })
+
     it('should return false for ineligible secondary leave week', function () {
       const result = weeks._weekEligibleForSecondaryLeave({ secondary: { leave: { text: 'paternity' } } })
+
+      assert.isFalse(result)
+    })
+
+    it('should return false for ineligible secondary statutory leave week', function () {
+      const result = weeks2._weekEligibleForSecondaryLeave({ secondary: { leave: { text: 'paternity' } } })
+
+      assert.isFalse(result)
+    })
+
+    it('should return true for eligible primary pay week', function () {
+      const result = weeks2._weekEligibleForPrimaryPay({ primary: { leave: { text: 'maternity' } } })
+
+      assert.isTrue(result)
+    })
+  })
+
+  describe('Shared Pay or Leave methods', function () {
+    const weeks = new Weeks(options)
+
+    it('should return false for primary shared pay', function () {
+      const result = weeks.hasPrimarySharedPayOrLeave()
 
       assert.isFalse(result)
     })
