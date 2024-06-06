af9aa56dbc971d2f406f06e19d19eb37131b67fe
Aaron Lambley <aaron.lambley@gmail.com>
2024-06-06 15:25:53 +0100

    add fixture for selecting surrogacy in the first step

--- test/features/surrogacy/primary-leave-and-pay.spec.js
+++ test/features/surrogacy/primary-leave-and-pay.spec.js
@@ -1,86 +1,74 @@
-const { test, expect } = require('@playwright/test')
+const { expect } = require('@playwright/test');
+const test = require('../../fixtures/surrogacy/select-surrogacy');
 
 test.describe('when "surrogacy" is selected on "nature-of-parenthood"', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000')
-    await page.check("input[value='surrogacy']")
-    await page.click('button:text("Continue")')
-  })
+  test.beforeEach(async ({ setupSurrogacyPage }) => {});
 
-  test('should have url', async ({ page }) => {
-    await expect(page.url()).toEqual('http://localhost:3000/eligibility/parental-order-parent/shared-parental-leave-and-pay')
-  })
+  test('should have url', async ({ setupSurrogacyPage: page }) => {
+    await expect(page.url()).toBe('http://localhost:3000/eligibility/parental-order-parent/shared-parental-leave-and-pay');
+  });
 
-  test('should have title', async ({ page }) => {
+  test('should have title', async ({ setupSurrogacyPage: page }) => {
     await expect(
-      page.getByRole('heading', { name: 'Parental order parent’s leave and pay' })
-    ).toBeVisible()
-  })
+      await page.getByRole('heading', { name: 'Parental order parent’s leave and pay' })
+    ).toBeVisible();
+  });
 
   test.describe('with the form buttons', () => {
-    test('radio buttons should be clickable', async ({ page }) => {
-      await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click()
-      await expect(page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes')).toBeChecked()
+    test('radio buttons should be clickable', async ({ setupSurrogacyPage: page }) => {
+      await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click();
+      await expect(page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes')).toBeChecked();
 
+      await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click();
+      await expect(page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes')).toBeChecked();
+    });
 
-      await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click()
-      await expect(page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes')).toBeChecked()
-
-    })
-
-    test('continue button should be clickable', async ({ page }) => {
-      await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click()
-      await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click()
-
-      await page.click('button:text("Continue")')
+    test('continue button should be clickable', async ({ setupSurrogacyPage: page }) => {
+      await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click();
+      await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click();
+      await page.click('button:text("Continue")');
 
       await expect(
-        page.getByRole('heading', {
+        await page.getByRole('heading', {
           name: 'Partner’s leave and pay'
         })
-      ).toBeVisible()
+      ).toBeVisible();
+    });
 
-    })
+    test('should show error message when radio button is not selected', async ({ setupSurrogacyPage: page }) => {
+      await page.click('button:text("Continue")'); // <- Click on continue button without selecting any radio buttons first
 
-    test('should show error message when radio button is not selected', async ({ page }) => {
-      await page.click('button:text("Continue")') // <- Click on continue button without selecting any radio buttons first
+      await expect(await page.getByText('There is a problem')).toBeVisible(); // <- Displays an error
+      await expect(await page.getByRole('link', { name: 'Select whether you are eligible for Shared Parental Leave' })).toBeVisible(); // <- Displays an error
+      await expect(await page.getByRole('link', { name: 'Select whether you are eligible for Shared Parental Pay' })).toBeVisible(); // <- Displays an error
+    });
+  });
 
-      await expect(page.getByText('There is a problem')).toBeVisible() // <- Displays an error
-      await expect(page.getByRole('link', { name: 'Select whether you are eligible for Shared Parental Leave' })).toBeVisible() // <- Displays an error
-      await expect(page.getByRole('link', { name: 'Select whether you are eligible for Shared Parental Pay' })).toBeVisible() // <- Displays an error
-
-    })
-  })
-
-  test('correct page displays when next page buttons are both no', async ({ page }) => {
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('No').click()
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('No').click()
-    await page.click('button:text("Continue")')
+  test('correct page displays when next page buttons are both no', async ({ setupSurrogacyPage: page }) => {
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('No').click();
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('No').click();
+    await page.click('button:text("Continue")');
 
     await expect(
-      page.getByRole('heading', {
+      await page.getByRole('heading', {
         name: 'Parental order parent’s leave and pay'
       })
-    ).toBeVisible()
+    ).toBeVisible();
+  });
 
-  })
+  test('correct page displays when next page buttons are yes then no', async ({ setupSurrogacyPage: page }) => {
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click();
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('No').click();
+    await page.click('button:text("Continue")');
 
-  test('correct page displays when next page buttons are yes then no', async ({ page }) => {
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('Yes').click()
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('No').click()
-    await page.click('button:text("Continue")')
+    await expect(await page.getByText('Is the parental order parent eligible for Statutory Adoption Pay?')).toBeVisible();
+  });
 
-    await expect(page.getByText('Is the parental order parent eligible for Statutory Adoption Pay?')).toBeVisible()
+  test('correct page displays when next page buttons are no then yes', async ({ setupSurrogacyPage: page }) => {
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('No').click();
+    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click();
+    await page.click('button:text("Continue")');
 
-  })
-
-  test('correct page displays when next page buttons are no then yes', async ({ page }) => {
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Shared Parental Leave?' }).getByLabel('No').click()
-    await page.getByRole('group', { name: 'Is the parental order parent eligible for Statutory Shared Parental Pay?' }).getByLabel('Yes').click()
-    await page.click('button:text("Continue")')
-
-    await expect(page.getByText('Is the parental order parent eligible for Adoption Leave?')).toBeVisible()
-
-  })
-
-})
+    await expect(await page.getByText('Is the parental order parent eligible for Adoption Leave?')).toBeVisible();
+  });
+});
---
+++ test/fixtures/surrogacy/select-surrogacy.js
@@ -0,0 +1,12 @@
+const { test: base } = require('@playwright/test');
+
+const test = base.extend({
+  setupSurrogacyPage: async ({ page }, use) => {
+    await page.goto('http://localhost:3000');
+    await page.check("input[value='surrogacy']");
+    await page.click('button:text("Continue")');
+    await use(page);
+  }
+});
+
+module.exports = test;
