0f83625b3f6012304b80f010e6c3e967fbcc58df
louisware <louis.ware@solirius.com>
2024-08-15 14:35:16 +0100

    Implemented leave starts, length label, and notify employers label tests

--- test/features/birth/leaveSummary/leave-summary-all-eligible.spec.js
+++ test/features/birth/leaveSummary/leave-summary-all-eligible.spec.js
@@ -89,15 +89,54 @@ test.describe('Leave summary page', () => {
   })
 
   test.describe('Partners Leave Dates', () => {
-    test('paternity leave starts (week 1) label has correct value', async ({ setupLeavePage: page }) => { // <- Should be 3 months prior to test run date
+    test('paternity leave starts (week 1) label has correct value', async ({ setupLeavePage: page }) => { // <- Should be on the Monday 3 months prior to test run date
+      const today = new Date()
+
+      const threeMonthsAgo = new Date(today.setMonth(today.getMonth() - 3))
+      const day = threeMonthsAgo.getDate()
+      const month = threeMonthsAgo.toLocaleString('default', { month: 'long' }) // <- Get full month name (e.g. "September")
+      const year = threeMonthsAgo.getFullYear()
+      const dayOfTheWeek = (threeMonthsAgo.getDay() - 1) % 7 // <- 0: Sunday, 1: Monday, 2: Tuesday etc...
+  
+     const maternityLeaveStartsLabel = await page.textContent('#leave-summary > div > div > dl:nth-child(11) > div:nth-child(1) > dd')
+
+     const correctStartDate = `week starting ${day.toString() - dayOfTheWeek} ${month.toString()} ${year.toString()}` // <- Will be in format 'week starting 13 May 2024', for example
+
+     console.log("correct start date: ", correctStartDate)
+
+     expect(maternityLeaveStartsLabel).toContain(correctStartDate)
     
     })
   
     test('length label has correct value', async ({ setupLeavePage: page }) => { // <- Should be 2
+      const lengthLabel = await page.textContent('#leave-summary > div > div > dl:nth-child(11) > div:nth-child(2) > dd')
+
+      expect(lengthLabel).toContain("2 weeks")
       
     })
   
     test('notify employers label has correct value', async ({ setupLeavePage: page }) => { // <- 15 weeks before due date
+      const today = new Date()
+
+      const threeMonthsAgo = new Date(today.setMonth(today.getMonth() - 3))
+      const threeMonthsAgoDay = threeMonthsAgo.getDay()
+
+      const daystoMonday = (threeMonthsAgoDay === 0 ? -6 : 1) - threeMonthsAgoDay // Calculates the days to the Monday of the week as a difference 
+      const startOfWeek = new Date(threeMonthsAgo)
+      startOfWeek.setDate(threeMonthsAgo.getDate() + daystoMonday)
+
+      const fifteenweeksBefore = new Date(startOfWeek)
+      fifteenweeksBefore.setDate(startOfWeek.getDate() - (15 * 7)) // Includes the week that the maternity leave was started so 26 weeks added 
+
+      const month = fifteenweeksBefore.toLocaleString('default', { month: 'long' }) // <- Get full month name (e.g. "September")
+      const year = fifteenweeksBefore.getFullYear()
+      const day = fifteenweeksBefore.getDate()
+  
+      const notifyEmployerLabel = await page.textContent('#leave-summary > div > div > dl:nth-child(11) > div:nth-child(3) > dd')
+
+      const correctnotifyEmployerDate = `by ${day.toString()} ${month.toString()} ${year.toString()}` // <- Will be in format 'week starting 13 May 2024', for example
+
+      expect(notifyEmployerLabel).toContain(correctnotifyEmployerDate)
       
     })
 
