d34b3f09fe0c9f53a921b75ef42a44c5f323e8d5
Aaron Lambley <42571140+lambley@users.noreply.github.com>
2024-08-27 15:24:57 +0100

    lint: remove semicolons

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,5 @@
-require('core-js/stable');
-require('regenerator-runtime/runtime');
+require('core-js/stable')
+require('regenerator-runtime/runtime')
 
 window.planner = require('./planner')
 window.summary = require('./summary')
