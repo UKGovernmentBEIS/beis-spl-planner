c518522b1d415591b49024dd73b3a402fb5298dd
Aaron Lambley <aaron.lambley@gmail.com>
2024-09-06 10:53:01 +0100

    lint

--- test/features/fixtures/birth/planner/mother-eligible-partner-not-eligible.js
+++ test/features/fixtures/birth/planner/mother-eligible-partner-not-eligible.js
@@ -1,74 +1,74 @@
-const { test: base } = require("@playwright/test");
+const { test: base } = require('@playwright/test')
 
 const test = base.extend({
   setupPlannerPage: async ({ page, baseURL }, use) => {
     // nature-of-parenthood
-    await page.goto(`${baseURL}`);
-    await page.check("input[value='birth']");
-    await page.click('button:text("Continue")');
+    await page.goto(`${baseURL}`)
+    await page.check("input[value='birth']")
+    await page.click('button:text("Continue")')
 
     // eligibility/mother/shared-parental-leave-and-pay
     await page
-      .getByRole("group", {
-        name: "Is the mother eligible for Shared Parental Leave?",
+      .getByRole('group', {
+        name: 'Is the mother eligible for Shared Parental Leave?'
       })
-      .getByLabel("Yes")
-      .click();
+      .getByLabel('Yes')
+      .click()
     await page
-      .getByRole("group", {
-        name: "Is the mother eligible for Statutory Shared Parental Pay?",
+      .getByRole('group', {
+        name: 'Is the mother eligible for Statutory Shared Parental Pay?'
       })
-      .getByLabel("Yes")
-      .click();
-    await page.click('button:text("Continue")');
+      .getByLabel('Yes')
+      .click()
+    await page.click('button:text("Continue")')
 
     // eligibility/partner/shared-parental-leave-and-pay
     await page
-      .getByRole("group", {
-        name: "Is the partner eligible for Shared Parental Leave?",
+      .getByRole('group', {
+        name: 'Is the partner eligible for Shared Parental Leave?'
       })
-      .getByLabel("No")
-      .click();
+      .getByLabel('No')
+      .click()
     await page
-      .getByRole("group", {
-        name: "Is the partner eligible for Statutory Shared Parental Pay?",
+      .getByRole('group', {
+        name: 'Is the partner eligible for Statutory Shared Parental Pay?'
       })
-      .getByLabel("No")
-      .click();
-    await page.click('button:text("Continue")');
+      .getByLabel('No')
+      .click()
+    await page.click('button:text("Continue")')
 
     // eligibility/partner/paternity-leave-and-pay
     await page
-      .getByRole("group", {
-        name: "Is the partner eligible for Paternity Leave?",
+      .getByRole('group', {
+        name: 'Is the partner eligible for Paternity Leave?'
       })
-      .getByLabel("No")
-      .click();
+      .getByLabel('No')
+      .click()
     await page
-      .getByRole("group", {
-        name: "Is the partner eligible for Statutory Paternity Pay?",
+      .getByRole('group', {
+        name: 'Is the partner eligible for Statutory Paternity Pay?'
       })
-      .getByLabel("No")
-      .click();
-    await page.click('button:text("Continue")');
+      .getByLabel('No')
+      .click()
+    await page.click('button:text("Continue")')
 
     // start-date
-    const today = new Date();
-    const threeMonthsAgo = new Date(today.setMonth(today.getMonth() - 3));
-    const day = threeMonthsAgo.getDate();
-    const month = threeMonthsAgo.getMonth() + 1; // <- Months are zero-based, so add 1
-    const year = threeMonthsAgo.getFullYear();
+    const today = new Date()
+    const threeMonthsAgo = new Date(today.setMonth(today.getMonth() - 3))
+    const day = threeMonthsAgo.getDate()
+    const month = threeMonthsAgo.getMonth() + 1 // <- Months are zero-based, so add 1
+    const year = threeMonthsAgo.getFullYear()
 
-    await page.getByRole("textbox", { name: "Day" }).fill(day.toString());
-    await page.getByRole("textbox", { name: "Month" }).fill(month.toString());
-    await page.getByRole("textbox", { name: "Year" }).fill(year.toString());
+    await page.getByRole('textbox', { name: 'Day' }).fill(day.toString())
+    await page.getByRole('textbox', { name: 'Month' }).fill(month.toString())
+    await page.getByRole('textbox', { name: 'Year' }).fill(year.toString())
 
-    await page.click('button:text("Continue")');
+    await page.click('button:text("Continue")')
 
     // parent-salaries
-    await page.getByRole("link", { name: "Skip this question" }).click();
-    await use(page);
-  },
-});
+    await page.getByRole('link', { name: 'Skip this question' }).click()
+    await use(page)
+  }
+})
 
-module.exports = test;
+module.exports = test
--- test/features/utils/selectors/planner.js
+++ test/features/utils/selectors/planner.js
@@ -1,14 +1,14 @@
 const plannerSelectors = {
-  remainingLeaveSidebar: "#sidebar-information",
+  remainingLeaveSidebar: '#sidebar-information',
   fathersLeaveCalendar:
-    "#calendar > table > tbody > tr:nth-child(21) > td.govuk-table__cell.leave.shared",
-  disabledCells: ".govuk-table__cell.leave.disabled",
+    '#calendar > table > tbody > tr:nth-child(21) > td.govuk-table__cell.leave.shared',
+  disabledCells: '.govuk-table__cell.leave.disabled',
   cellSelector: (week, parent) =>
-    `td[data-row="${week}"][data-column="${parent === "mother" ? 0 : 2}"]`,
+    `td[data-row="${week}"][data-column="${parent === 'mother' ? 0 : 2}"]`,
   notEligibleCellSelector: (week, parent) =>
     `td[headers~="week-${week}-date"][headers~="${
-      parent === "mother" ? "primary-leave" : "secondary-leave"
-    }"] .govuk-body-s.no-margin`,
-};
+      parent === 'mother' ? 'primary-leave' : 'secondary-leave'
+    }"] .govuk-body-s.no-margin`
+}
 
-module.exports = plannerSelectors;
+module.exports = plannerSelectors
