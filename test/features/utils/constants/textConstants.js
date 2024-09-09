ccfeaede1973ac2e6b05c97892d49eee05a31d1a
Aaron Lambley <aaron.lambley@gmail.com>
2024-09-09 15:59:38 +0100

    add surrogacy planner tests

--- test/features/utils/constants/textConstants.js
+++ test/features/utils/constants/textConstants.js
@@ -7,6 +7,8 @@ const textConstants = {
     'Neither parent is eligible for Shared Parental Leave or Pay',
   compulsoryAdoptionLeaveNotice:
     'You must take at least 2 weeks of Adoption Leave or Pay and it must include the first week the child lives with you.',
+  compulsorySurrogacyLeaveNotice:
+    'You must take at least 2 weeks of Adoption Leave or Pay and it must include the first week the child lives with you.',
   notEligibleText: 'Not eligible for leave or pay'
 }
 
