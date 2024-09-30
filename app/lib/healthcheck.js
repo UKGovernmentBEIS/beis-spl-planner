0d7d5aa5178e92171bc96714207c7127b316c22a
Aaron Lambley <42571140+lambley@users.noreply.github.com>
2024-09-30 09:30:34 +0100

    add healthcheck endpoint (#266)
    * add healthcheck endpoint
    
    * add xml declarion to healthcheck response

---
+++ app/lib/healthcheck.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const xml = require('xml')
+
+router.get('/pingdom/ping.xml', (req, res) => {
+  const healthStatus = {
+    app: { success: true }
+  }
+
+  const isHealthy = Object.values(healthStatus).every((check) => check.success)
+  const status = isHealthy ? 'OK' : 'FAIL'
+
+  const response = xml([{ pingdom_http_custom_check: [{ status }] }], {
+    declaration: true
+  })
+
+  console.info(`Healthcheck status: ${status}`)
+
+  res.type('application/xml')
+  res.status(isHealthy ? 200 : 500).send(response)
+})
+
+module.exports = router
---
+++ test/unit/lib/healthcheck.test.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const request = require('supertest')
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const healthcheckRouter = require('../../../app/lib/healthcheck')
+
+const app = express()
+app.use(healthcheckRouter)
+
+describe('Health Check Endpoint', () => {
+  describe('When the service is healthy', () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/pingdom/ping.xml')
+      expect(response.status).to.equal(200)
+    })
+
+    it('should return correctly formed XML', async () => {
+      const response = await request(app).get('/pingdom/ping.xml')
+      expect(response.type).to.equal('application/xml')
+
+      const expectedXml =
+        '<?xml version="1.0" encoding="UTF-8"?><pingdom_http_custom_check><status>OK</status></pingdom_http_custom_check>'
+
+      const actualXml = response.text.trim()
+      expect(actualXml).to.equal(expectedXml)
+    })
+  })
+})
