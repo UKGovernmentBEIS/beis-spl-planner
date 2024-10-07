ba9f434387dc6aeaadd31a946b75bff56a6c6cdb
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-07 13:54:44 +0100

    split unit tests into app and common folders

---
+++ test/unit/app/router.test.js
@@ -0,0 +1,15 @@
+const supertest = require("supertest");
+const { describe, it } = require("mocha");
+
+const getApp = require("../../../server").getApp;
+const paths = require("../../../app/paths");
+
+describe("GET /", () => {
+  it("returns 302 status and redirects to the first question page", (done) => {
+    supertest(getApp())
+      .get(paths.getPath("root"))
+      .expect(302)
+      .expect("Location", paths.getPath("natureOfParenthood"))
+      .end(done);
+  });
+});
