81e91d2c2cf1366d85c939193e8cc2814499cb71
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-08 09:47:54 +0100

    lint

--- test/unit/app/logger.test.js
+++ test/unit/app/logger.test.js
@@ -1,104 +1,104 @@
-const { xdescribe, describe, it, beforeEach, afterEach } = require("mocha");
-const stdMocks = require("std-mocks");
-const sinon = require("sinon");
-const chai = require("chai");
-const expect = chai.expect;
+const { xdescribe, describe, it, beforeEach, afterEach } = require('mocha')
+const stdMocks = require('std-mocks')
+const sinon = require('sinon')
+const chai = require('chai')
+const expect = chai.expect
 
 const expectedInfo = {
-  EventMessage: "Message for info",
+  EventMessage: 'Message for info',
   EventCount: 1,
-  EventType: "ApplicationEvent",
-  EventResult: "NA",
-  EventSeverity: "Informational",
-  EventOriginalSeverity: "info",
-  EventSchema: "ProcessEvent",
-  EventSchemaVersion: "0.1.4",
-  ActingAppType: "Express",
-  AdditionalFields: { CustomASIMFormatter: true, TraceHeaders: {} },
-};
+  EventType: 'ApplicationEvent',
+  EventResult: 'NA',
+  EventSeverity: 'Informational',
+  EventOriginalSeverity: 'info',
+  EventSchema: 'ProcessEvent',
+  EventSchemaVersion: '0.1.4',
+  ActingAppType: 'Express',
+  AdditionalFields: { CustomASIMFormatter: true, TraceHeaders: {} }
+}
 
 const expectedError = {
-  EventMessage: "Message for error",
+  EventMessage: 'Message for error',
   EventCount: 1,
-  EventType: "ApplicationEvent",
-  EventResult: "NA",
-  EventSeverity: "Medium",
-  EventOriginalSeverity: "error",
-  EventSchema: "ProcessEvent",
-  EventSchemaVersion: "0.1.4",
-  ActingAppType: "Express",
-  AdditionalFields: { CustomASIMFormatter: true, TraceHeaders: {} },
-};
+  EventType: 'ApplicationEvent',
+  EventResult: 'NA',
+  EventSeverity: 'Medium',
+  EventOriginalSeverity: 'error',
+  EventSchema: 'ProcessEvent',
+  EventSchemaVersion: '0.1.4',
+  ActingAppType: 'Express',
+  AdditionalFields: { CustomASIMFormatter: true, TraceHeaders: {} }
+}
 
-describe("Logger transport check", () => {
-  let logger;
+describe('Logger transport check', () => {
+  let logger
 
   afterEach(() => {
-    sinon.restore();
-    stdMocks.restore();
-  });
+    sinon.restore()
+    stdMocks.restore()
+  })
 
   // tests pass when running separately but fail on CI and with the whole test suite, due to env setting
-  xdescribe("In production environment", () => {
+  xdescribe('In production environment', () => {
     beforeEach(() => {
-      stdMocks.use();
-      process.env.NODE_ENV = "production";
-      logger = require("../../test-app");
-    });
+      stdMocks.use()
+      process.env.NODE_ENV = 'production'
+      logger = require('../../test-app')
+    })
 
-    it("should log info in JSON format", function () {
+    it('should log info in JSON format', function () {
       if (process.env.CI) {
-        stdMocks.restore();
-        this.skip();
+        stdMocks.restore()
+        this.skip()
       }
-      logger.info("Message for info", { eventType: "ApplicationEvent" });
+      logger.info('Message for info', { eventType: 'ApplicationEvent' })
 
-      stdMocks.restore();
-      const output = stdMocks.flush();
+      stdMocks.restore()
+      const output = stdMocks.flush()
 
-      const logEntry = JSON.parse(output.stdout[0]);
+      const logEntry = JSON.parse(output.stdout[0])
 
-      expect(logEntry).to.deep.include(expectedInfo);
-    });
-    it("should log error in JSON format", function () {
+      expect(logEntry).to.deep.include(expectedInfo)
+    })
+    it('should log error in JSON format', function () {
       if (process.env.CI) {
-        stdMocks.restore();
-        this.skip();
+        stdMocks.restore()
+        this.skip()
       }
-      logger.error("Message for error", { eventType: "ApplicationEvent" });
+      logger.error('Message for error', { eventType: 'ApplicationEvent' })
 
-      stdMocks.restore();
-      const output = stdMocks.flush();
+      stdMocks.restore()
+      const output = stdMocks.flush()
 
-      const logEntry = JSON.parse(output.stdout[0]);
+      const logEntry = JSON.parse(output.stdout[0])
 
-      expect(logEntry).to.deep.include(expectedError);
-    });
-  });
+      expect(logEntry).to.deep.include(expectedError)
+    })
+  })
 
-  describe("In non-production environment", () => {
+  describe('In non-production environment', () => {
     beforeEach(() => {
-      process.env.NODE_ENV = "development";
-      logger = require("../../test-app");
-      stdMocks.use();
-    });
+      process.env.NODE_ENV = 'development'
+      logger = require('../../test-app')
+      stdMocks.use()
+    })
 
-    it("should log info in plain text format", () => {
-      logger.info("Plain log info");
+    it('should log info in plain text format', () => {
+      logger.info('Plain log info')
 
-      stdMocks.restore();
-      const output = stdMocks.flush();
+      stdMocks.restore()
+      const output = stdMocks.flush()
 
-      expect(output.stdout[0]).to.contain("Plain log info");
-    });
+      expect(output.stdout[0]).to.contain('Plain log info')
+    })
 
-    it("should log error in plain text format", () => {
-      logger.error("Plain log error");
+    it('should log error in plain text format', () => {
+      logger.error('Plain log error')
 
-      stdMocks.restore();
-      const output = stdMocks.flush();
+      stdMocks.restore()
+      const output = stdMocks.flush()
 
-      expect(output.stdout[0]).to.contain("Plain log error");
-    });
-  });
-});
+      expect(output.stdout[0]).to.contain('Plain log error')
+    })
+  })
+})
--- test/unit/app/node-email.test.js
+++ test/unit/app/node-email.test.js
@@ -1,69 +1,69 @@
-const { describe, it, beforeEach, afterEach } = require("mocha");
-const sinon = require("sinon");
-const chai = require("chai");
-const expect = chai.expect;
-const nodemailer = require("nodemailer");
-const sendMail = require("../../../app/node-email");
+const { describe, it, beforeEach, afterEach } = require('mocha')
+const sinon = require('sinon')
+const chai = require('chai')
+const expect = chai.expect
+const nodemailer = require('nodemailer')
+const sendMail = require('../../../app/node-email')
 
-describe("sendMail", function () {
-  let createTransportStub, sendMailStub;
+describe('sendMail', function () {
+  let createTransportStub, sendMailStub
 
   beforeEach(() => {
     sendMailStub = sinon
       .stub()
-      .yields(null, { response: "250 OK: Message accepted" });
-    createTransportStub = sinon.stub(nodemailer, "createTransport").returns({
-      sendMail: sendMailStub,
-    });
-  });
+      .yields(null, { response: '250 OK: Message accepted' })
+    createTransportStub = sinon.stub(nodemailer, 'createTransport').returns({
+      sendMail: sendMailStub
+    })
+  })
 
   afterEach(() => {
-    sinon.restore();
-  });
+    sinon.restore()
+  })
 
-  describe("successful email sending", function () {
-    const experience = "Great service!";
-    const moreDetails = "No additional feedback";
+  describe('successful email sending', function () {
+    const experience = 'Great service!'
+    const moreDetails = 'No additional feedback'
 
-    it("should call createTransport once", async function () {
-      await sendMail(experience, moreDetails);
-      expect(createTransportStub.calledOnce).to.equal(true);
-    });
+    it('should call createTransport once', async function () {
+      await sendMail(experience, moreDetails)
+      expect(createTransportStub.calledOnce).to.equal(true)
+    })
 
-    it("should call sendMail once", async function () {
-      await sendMail(experience, moreDetails);
-      expect(sendMailStub.calledOnce).to.equal(true);
-    });
+    it('should call sendMail once', async function () {
+      await sendMail(experience, moreDetails)
+      expect(sendMailStub.calledOnce).to.equal(true)
+    })
 
-    it("should set the correct email options", async function () {
-      await sendMail(experience, moreDetails);
-      const mailOptions = sendMailStub.getCall(0).args[0];
+    it('should set the correct email options', async function () {
+      await sendMail(experience, moreDetails)
+      const mailOptions = sendMailStub.getCall(0).args[0]
 
       expect(mailOptions.from).to.equal(
         `SPL Feedback <${process.env.EMAIL_AUTH_USER}>`
-      );
-      expect(mailOptions.to).to.equal(process.env.EMAIL_RECIPIENT);
-      expect(mailOptions.subject).to.equal("SPL Planner Feedback");
-      expect(mailOptions.text).to.contain(experience);
-      expect(mailOptions.text).to.contain(moreDetails);
-    });
+      )
+      expect(mailOptions.to).to.equal(process.env.EMAIL_RECIPIENT)
+      expect(mailOptions.subject).to.equal('SPL Planner Feedback')
+      expect(mailOptions.text).to.contain(experience)
+      expect(mailOptions.text).to.contain(moreDetails)
+    })
 
-    it("should have the correctly formatted text", async function () {
+    it('should have the correctly formatted text', async function () {
       const expectedText = `
       What was your experience of the service:
       ${experience}
 
       How could we improve this service:
       ${moreDetails}
-      `;
+      `
 
-      await sendMail(experience, moreDetails);
-      const mailOptions = sendMailStub.getCall(0).args[0];
+      await sendMail(experience, moreDetails)
+      const mailOptions = sendMailStub.getCall(0).args[0]
 
       expect(mailOptions.from).to.equal(
         `SPL Feedback <${process.env.EMAIL_AUTH_USER}>`
-      );
-      expect(mailOptions.text).to.eq(expectedText);
-    });
-  });
-});
+      )
+      expect(mailOptions.text).to.eq(expectedText)
+    })
+  })
+})
