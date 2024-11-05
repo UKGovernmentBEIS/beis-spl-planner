0b8476ab91ae736fbcafda30eca115cb6ec51131
Devin Rathod <119806596+devind-ra@users.noreply.github.com>
2024-11-05 16:53:43 +0000

    SPL-464 - Update cookie banner (#281)
    * updating cookie banner
    
    * fixing issue with accessibility
    
    * removing test code in progress
    
    * removing extra cookie banner work and changing structure of header
    
    * removing extra cookie text
    
    * change SERVICE_NAME to title case
    
    * remove cookie banner padding
    
    * checking cookie banner link and text displayed
    
    * fixing lint errors
    
    * fixing accept and reject cookies, adding tests to check cookies correctly
    
    ---------
    
    Co-authored-by: Aaron Lambley <aaron.lambley@gmail.com>

--- test/features/nature-of-parenthood.spec.js
+++ test/features/nature-of-parenthood.spec.js
@@ -59,4 +59,34 @@ test.describe('nature-of-parenthood', () => {
 
     await expect(page.getByText('There is a problem')).toBeVisible() // <- Displays an error
   })
+
+  test.describe('cookie-banner', () => {
+    test.beforeEach(async ({ page }) => {
+      const { baseURL } = page.context()._options
+      await page.goto(`${baseURL}`)
+    })
+
+    test('should display text on cookie banner', async ({ page }) => {
+      await expect(page.getByText('Cookies on Plan Shared Parental Leave and Pay')).toBeVisible()
+    })
+
+    test('should have analytical cookies when accept button pressed', async ({ page }) => {
+      await page.click('text="Accept additional cookies"')
+      const cookies = await page.context().cookies()
+      const analyticalCookies = cookies.find(cookie => cookie.name === 'cm-user-preferences')
+      expect(analyticalCookies.value).toBe('%7B%22analytics%22%3A%22on%22%7D')
+    })
+
+    test('should not have analytical cookies when reject button pressed', async ({ page }) => {
+      await page.click('text="Reject additional cookies"')
+      const cookies = await page.context().cookies()
+      const analyticalCookies = cookies.find(cookie => cookie.name === 'cm-user-preferences')
+      expect(analyticalCookies.value).toBe('%7B%22analytics%22%3A%22off%22%7D')
+    })
+
+    test('should go to cookies page when pressing view cookies link', async ({ page }) => {
+      await page.click('text="View cookies"')
+      await expect(page).toHaveTitle(/Cookies - Plan Shared Parental Leave and Pay - GOV.UK/)
+    })
+  })
 })
