c98dda73667594e18776f0e386b83fcc0d252e37
Aaron Lambley <aaron.lambley@gmail.com>
2024-11-22 11:02:41 +0000

    Update gruntfile.js to use environment variable for NODE_ENV and add compile:prod script in package.json

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
   const env = {
     dev: {
       GOOGLE_ANALYTICS_ID: 'GTM-NJ98WRPX',
-      NODE_ENV: 'development'
+      NODE_ENV: process.env.NODE_ENV || 'development'
     }
   }
 
@@ -141,7 +141,7 @@ module.exports = function (grunt) {
           .transform(
             // Required in order to process node_modules files
             { global: true },
-            envify({ NODE_ENV: 'production' })
+            envify({ NODE_ENV: process.env.NODE_ENV })
           )
           .bundle()
       }
