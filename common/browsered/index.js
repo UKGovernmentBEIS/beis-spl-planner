d497fa682c1e0312feb5a217a2a57f1a6debe061
Aaron Lambley <aaron.lambley@gmail.com>
2024-12-18 16:30:11 +0000

    initial commit to refactor cookie banner to use use updated cookies

--- common/browsered/index.js
+++ common/browsered/index.js
@@ -1,41 +1,66 @@
-window.GOVUKFrontend = require('../../node_modules/govuk-frontend/dist/govuk/all.bundle.js')
-window.GOVUKFrontend.initAll()
+const { bindAll } = require("lodash");
 
-const cookieManager = require('../../node_modules/@dvsa/cookie-manager/cookie-manager.js')
-cookieManager.init({
-  'cookie-banner-id': 'cookie_banner',
-  'cookie-banner-saved-callback': function () {
-    const banner = document.querySelector('#cookie_banner')
-    banner.hidden = true
-  },
-  'cookie-banner-visible-on-page-with-preference-form': false,
-  'user-preference-configuration-form-id': 'cm_user_preference_form',
-  'user-preference-saved-callback': function () {
-    const message = document.querySelector('#cookie-preference-success')
-    message.style.display = 'block'
-    document.body.scrollTop = 0 // For Safari
-    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
-  },
-  'set-checkboxes-in-preference-form': true,
-  'cookie-manifest': [{
-    'category-name': 'essential',
-    optional: false,
-    cookies: [
-      'application'
-    ]
-  },
-  {
-    'category-name': 'analytics',
-    optional: true,
-    cookies: [
-      '_ga',
-      '_ga_NJ98WRPX',
-      '_gat',
-      '_gat_UA-158688524-1',
-      '_gid'
-    ]
+window.GOVUKFrontend = require("../../node_modules/govuk-frontend/dist/govuk/all.bundle.js");
+window.GOVUKFrontend.initAll();
+
+// cookie banner logic
+const cookiesPolicyName = "cookies_policy";
+
+// bind event listeners to accept, reject buttons
+function bindAcceptEventListener() {
+  const acceptButton = document.querySelector(".cookie-banner-accept-button");
+
+  try {
+    acceptButton.addEventListener("click", function () {
+      console.log("accept button clicked");
+    });
+  } catch (error) {
+    console.error("error binding accept event listener", error);
   }
-  ]
-})
+}
 
-require('../../app/assets/javascripts/index')
+function bindRejectEventListener() {
+  const rejectButton = document.querySelector(".cookie-banner-reject-button");
+
+  try {
+    rejectButton.addEventListener("click", function () {
+      console.log("reject button clicked");
+    });
+  } catch (error) {
+    console.error("error binding reject event listener", error);
+  }
+}
+
+function bindAllEventListeners() {
+  bindAcceptEventListener();
+  bindRejectEventListener();
+}
+
+// set default cookie settings
+function getDefaultPolicy() {
+  return {
+    essential: true,
+    settings: false,
+    usage: false,
+    campaigns: false,
+  };
+}
+
+// logic to show cookie banner
+// TODO: method that checks if js is enabled
+
+// get cookie settings from cookie
+// TODO: method to get all cookies
+// TODO: method to check if cookie_preferences_set exists (and if so checks cookies_policy)
+  // if false, show cookie banner
+  // if true, hide cookie banner
+
+// set cookie settings based on event listeners
+// TODO: based on the button that is selected, updates the cookie_preferences_set and cookies_policy cookie
+
+// TODO: create method to call setup methods, bind event listeners, and check cookie settings
+
+// just for testing purposes
+bindAllEventListeners();
+
+require("../../app/assets/javascripts/index");
