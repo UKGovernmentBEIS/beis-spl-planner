ab8b08f488c547142f9104bb5b7b0953d809e700
Jahir <jahir.uddin@digital.trade.gov.uk>
2025-04-25 10:20:25 +0100

    added static assets compilation to image build

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,5 +2,7 @@
 
 # Exit early if something goes wrong
 set -e
+npx grunt generate-assets
+echo "Static assets generated successfully."
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
