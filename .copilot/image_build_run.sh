43b0b48996ae966523fb5c0dc88588af72b6fdbb
Jahir <jahir.uddin@digital.trade.gov.uk>
2025-04-25 11:07:46 +0100

    Integrate Grunt asset build into Paketo lifecycle

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,8 +2,5 @@
 
 # Exit early if something goes wrong
 set -e
-npm install
-npx grunt generate-assets
-echo "Static assets generated successfully."
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
